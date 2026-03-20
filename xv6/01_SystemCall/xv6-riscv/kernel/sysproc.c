262cc8f8da3dca8c47ef6c30971cac66a5fd6d44
imjeesoo <wltn446700@gmail.com>
2026-03-20 13:39:28 +0900

    Add sys_hello function for custom greeting

--- xv6/01_SystemCall/xv6-riscv/kernel/sysproc.c
+++ xv6/01_SystemCall/xv6-riscv/kernel/sysproc.c
@@ -108,9 +108,11 @@ sys_uptime(void)
   return xticks;
 }
 
+//--- MY IMPLEMENTATION START---
 uint64
 sys_hello(void)
 {
   printf("Hello, It's my first xv6 project!\n");
   return 0;
 }
+//--- MY IMPLEMENTATION END---
